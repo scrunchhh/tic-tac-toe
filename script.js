c7f9b4bd3376de747974aba72893f67fef92bc8c
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-29 17:31:12 -0500

    Added double click prevention

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ const Turn = () => {
   let turn = 0;
   return () => {
     turn += 1;
+    console.log(turn);
     return turn;
   };
+};
+
+const MarkX = (e) => {
+  const markX = document.createElement('img');
+  markX.setAttribute('src', 'images/x.svg');
+  e.target.appendChild(markX);
+};
+
+const MarkO = (e) => {
+  const markO = document.createElement('img');
+  markO.setAttribute('src', 'images/circle.svg');
+  e.target.appendChild(markO);
 };
 
 const turns = Turn();
 
 const Tile = () => {
   const clickTile = (e) => {
-    const markX = document.createElement('img');
-    markX.setAttribute('src', 'images/x.svg');
-    const markO = document.createElement('img');
-    markO.setAttribute('src', 'images/circle.svg');
-    if (turns() % 2 !== 0) {
-      e.target.appendChild(markX);
+    if (e.target.tagName === 'IMG' || e.target.firstChild) {
+      console.log('Already Clicked');
+    } else if (turns() % 2 !== 0) {
+      MarkX(e);
     } else {
-      e.target.appendChild(markO);
+      MarkO(e);
     }
   };
   return { clickTile };
